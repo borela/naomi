d946158553978daeb176f22deed3efccb6cad157
Alexandre Borela <alexandre.borela@gmail.com>
2018-08-23 15:21:01 -0300

    Update keymap build to include headers

--- commands/BuildKeymaps.py
+++ commands/BuildKeymaps.py
@@ -10,45 +10,32 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-from Naomi.plugin.paths import KEYMAPS_BUILD_DIR, KEYMAPS_SRC_DIR
-from os import makedirs, walk
-from os.path import dirname, exists, join
+from Naomi.system.fs import (
+    deleteDir,
+    ensureDirExists,
+    listFiles,
+    loadYaml,
+    toJsonString,
+    writeFile,
+)
+
+from Naomi.system.paths import (
+    KEYMAPS_BUILD_DIR,
+    KEYMAPS_SRC_DIR,
+)
+
+from Naomi.system.headers import KEYMAP as KEYMAP_HEADER
 from sublime_plugin import ApplicationCommand
-from ruamel.yaml import load
-from json import dump
-from shutil import rmtree
-
-
-def deleteBuildDir():
-    if not exists(KEYMAPS_BUILD_DIR):
-        return
-    rmtree(KEYMAPS_BUILD_DIR)
-
-
-def loadYaml(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return load(file)
-
-
-def writeJson(file_path, data):
-    # Make sure directory exits.
-    directory = dirname(file_path)
-    if not exists(directory):
-        makedirs(directory)
-
-    with open(file_path, 'w', encoding='utf-8', newline='\n') as file:
-        return dump(data, file, indent=2, sort_keys=True)
 
 
 class NaomiBuildKeymapsCommand(ApplicationCommand):
     def run(self):
-        deleteBuildDir()
-        for path, directories, files in walk(KEYMAPS_SRC_DIR):
-            for file in files:
-                SOURCE = join(path, file)
-                DESTINATION = join(
-                    path.replace('src', 'build'),
-                    file.replace('.yml', '.json')
-                )
-                data = loadYaml(SOURCE)
-                writeJson(DESTINATION, data)
+        deleteDir(KEYMAPS_BUILD_DIR)
+        for file in listFiles(KEYMAPS_SRC_DIR):
+            destination = file
+            destination = destination.replace('src', 'build')
+            destination = destination.replace('.yml', '.json')
+
+            yamlData = loadYaml(file)
+            jsonString = toJsonString(yamlData)
+            writeFile(destination, KEYMAP_HEADER + jsonString)

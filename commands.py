7f65a82c73bfae7fa5ed9aece650d72ecd381a70
Alexandre Borela <alexandre.borela@gmail.com>
2018-12-26 20:13:42 -0200

    Move entry point

--- commands.py
+++ commands.py
@@ -10,8 +10,6 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-from .system.event_bus import *
-
 from .system.commands.BuildCommands import * # noqa
 from .system.commands.BuildKeymaps import * # noqa
 from .system.commands.BuildMenus import * # noqa

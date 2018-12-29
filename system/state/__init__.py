cd6cc331bd8df94a841ee48f569ba8cefd050b55
Alexandre Borela <alexandre.borela@gmail.com>
2018-12-29 11:14:50 -0200

    Remove dead code

--- system/state/__init__.py
+++ system/state/__init__.py
@@ -10,12 +10,11 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-from Naomi.system.settings import get_settings
-from Naomi.system.events import settings_updated
-
 from .Store import Store
 from .reducers.directories import reducer as directories_reducer
 from .reducers.settings import reducer as settings_reducer
+
+from Naomi.system.event_bus import EVENT_BUS
 
 
 STORE = Store(
@@ -23,14 +22,4 @@ STORE = Store(
     settings=settings_reducer,
 )
 
-
-def plugin_loaded():
-    def update_settings_state():
-        SETTINGS = get_settings()
-        STORE.apply(settings_updated(SETTINGS))
-
-    SETTINGS = get_settings()
-    SETTINGS.clear_on_change('naomi-settings-state')
-    SETTINGS.add_on_change('naomi-settings-state', update_settings_state)
-
-    update_settings_state()
+EVENT_BUS.subscribe(lambda event: STORE.apply(event))

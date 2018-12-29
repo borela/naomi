86e2df1ddad468a2b56317dd2634f9189e486d96
Alexandre Borela <alexandre.borela@gmail.com>
2018-12-29 11:14:50 -0200

    Rename topic to message type

--- system/Bus.py
+++ system/Bus.py
@@ -18,8 +18,9 @@ class Bus:
         for listener in all_listeners:
             listener(message)
 
-        topic = message['topic']
-        specific_listeners = self.__listeners['specific'].get(topic, None)
+        message_type = message['type']
+        specific_listeners = self.__listeners['specific']
+        specific_listeners = specific_listeners.get(message_type, None)
 
         if specific_listeners is not None:
             for listener in specific_listeners:
@@ -27,37 +28,37 @@ class Bus:
 
 
     def subscribe(self, *args, **kwargs):
-        topic = None
+        message_type = None
         callback = None
 
         if kwargs:
-            topic = kwargs.get('topic', None)
+            message_type = kwargs.get('type', None)
             callback = kwargs.get('callback', None)
         elif args:
             if len(args) < 2:
                 callback = args[0]
             else:
-                topic = args[0]
+                message_type = args[0]
                 callback = args[1]
 
-        return self._subscribe(topic, callback)
+        return self._subscribe(message_type, callback)
 
 
-    def _subscribe(self, topic, callback):
+    def _subscribe(self, message_type, callback):
         if not callable(callback):
             raise ValueError('Invalid callback.')
 
-        if topic is None:
+        if message_type is None:
             self.__listeners['all'].append(callback)
             return
 
-        if topic not in self.__listeners['specific']:
-            self.__listeners['specific'][topic] = []
+        if message_type not in self.__listeners['specific']:
+            self.__listeners['specific'][message_type] = []
 
-        self.__listeners['specific'][topic].append(callback)
+        self.__listeners['specific'][message_type].append(callback)
 
 
-    """Stores listeners for specific topics and all of them."""
+    """Stores listeners for specific message types and all of them."""
     __listeners = {
         'all': [],
         'specific': {},

22a03a53b721e66213f7ac2caa7f2cc701cdf133
Alexandre Borela <alexandre.borela@gmail.com>
2019-06-29 15:03:16 -0300

    Transform utils into a package

---
+++ system/utils/__init__.py
@@ -0,0 +1,44 @@
+# Licensed under the Apache License, Version 2.0 (the “License”); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+from .delete_dir import delete_dir
+from .delete_dir_contents import delete_dir_contents
+from .dict_to_plist_array import dict_to_plist_array
+from .ensure_dir_exists import ensure_dir_exists
+from .EventEngine import EventEngine
+from .EventSubscription import EventSubscription
+from .list_files import list_files
+from .load_yaml import load_yaml
+from .plist_array_to_xml import plist_array_to_xml
+from .read_text_file import read_text_file
+from .StateStore import StateStore
+from .to_json_string import to_json_string
+from .to_xml_string import to_xml_string
+from .write_text_file import write_text_file
+
+
+__all__ = [
+    "delete_dir",
+    "delete_dir_contents",
+    "dict_to_plist_array",
+    "ensure_dir_exists",
+    "EventEngine",
+    "EventSubscription",
+    "list_files",
+    "load_yaml",
+    "plist_array_to_xml",
+    "read_text_file",
+    "StateStore",
+    "to_json_string",
+    "to_xml_string",
+    "write_text_file",
+]

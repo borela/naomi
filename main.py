8556bbf961fcef79483dcab0748668b561c46f24
Alexandre Borela <alexandre.borela@gmail.com>
2019-11-02 19:10:55 -0300

    Cleanup log message subscription logic.

--- main.py
+++ main.py
@@ -19,28 +19,20 @@ from sublime import load_settings
 from Naomi.system.state import STORE
 from Naomi.system.event_bus import EVENT_BUS
 
+LOG_EVENT_SUBSCRIPTION = EVENT_BUS.on(
+    event=LOG_MESSAGE_ADDED,
+    subscriber=print_log_message,
+)
 
 def plugin_loaded():
-    # Print log messages sent to the event bus.
-    EVENT_BUS.on(
-        event=LOG_MESSAGE_ADDED,
-        subscriber=print_log_message,
-    )
-
+    LOG_EVENT_SUBSCRIPTION.subscribe()
     SETTINGS = load_settings('Naomi.sublime-settings')
     SETTINGS.add_on_change('naomi-settings-state', update_settings)
-
     update_settings()
 
 def plugin_unloaded():
-    # EVENT_BUS.on(
-    #     type=LOG_MESSAGE_ADDED,
-    #     subscriber=print_log_message,
-    # )
-
     SETTINGS = load_settings('Naomi.sublime-settings')
     SETTINGS.clear_on_change('naomi-settings-state')
-
 
 def print_log_message(event):
     message = event['payload']['message']
@@ -51,7 +43,6 @@ def print_log_message(event):
 
     if message_level >= current_level:
         print('[Naomi][%s]: %s' % (level, message))
-
 
 def update_settings():
     SETTINGS = load_settings('Naomi.sublime-settings')

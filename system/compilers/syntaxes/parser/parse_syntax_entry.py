58a57d170ab743b7d7c25190f4742b050e523bf0
Alexandre Borela <alexandre@borela.tech>
2019-11-12 12:28:20 -0300

    Add logging messages.

--- system/compilers/syntaxes/parser/parse_syntax_entry.py
+++ system/compilers/syntaxes/parser/parse_syntax_entry.py
@@ -68,7 +68,11 @@ def parse_syntax_entry(settings):
 
     syntax.index_file(syntax)
 
+    log_debug('Parsing variables: %s' % syntax.package_relpath)
     parse_variables(syntax)
+
+    log_debug('Parsing contexts: %s' % syntax.package_relpath)
     parse_contexts(syntax)
 
+    log_info('Done compiling: %s' % syntax.package_relpath)
     return syntax

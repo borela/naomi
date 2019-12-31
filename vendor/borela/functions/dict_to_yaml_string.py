ce1342e75706a461b37794f65a6d87fdf72dc5fe
Alexandre Borela <alexandre.borela@gmail.com>
2019-12-31 12:40:59 -0300

    Remove omap tags from dump file.

--- vendor/borela/functions/dict_to_yaml_string.py
+++ vendor/borela/functions/dict_to_yaml_string.py
@@ -10,10 +10,24 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-from ruamel.yaml import (
-    dump,
-    RoundTripDumper,
-)
+from collections import OrderedDict
+from io import StringIO
+from ruamel.yaml import YAML
+from ruamel.yaml.scalarstring import walk_tree
 
 def dict_to_yaml_string(target_dict):
-    return dump(target_dict, Dumper=RoundTripDumper)
+    string_buffer = StringIO()
+
+    walk_tree(target_dict)
+
+    yaml = YAML()
+    yaml.version = (1, 2)
+    yaml.indent(mapping=2, sequence=4, offset=2)
+
+    yaml.Representer.add_representer(
+        OrderedDict,
+        yaml.Representer.represent_dict,
+    )
+
+    yaml.dump(target_dict, string_buffer)
+    return string_buffer.getvalue()
